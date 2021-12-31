e9c6e9868677da9089b32f571d4c00957bb3e157
Barend Koorzen <koorzenb@gmail.com>
2021-12-31 09:28:04 +0200

    Restarting data handling of project

--- test/dataHandler.test.js
+++ test/dataHandler.test.js
@@ -8,50 +8,17 @@ describe("DataHandler tests", () => {
 
     beforeAll(() => {
         dateTime = new DateTime({});
-    })
+    });
 
     beforeEach(() => {
         dataHandler = new DataHandler;
-    })
+    });
 
     afterEach(() => {
         dataHandler == null;
-    })
+    });
 
-    test("getEntry", () => {
-        expect(dataHandler.add(2,5)).toEqual(7)
-    })
+    test('should ', () => {
 
-    test("empty getEntry", () => {
-
-
-    })
-
-    /**
-     * Expect an objhect with weeknumber and {loggedTimes} that are populated with week info
-     */
-    test("Initialize emptyWeek", () => {
-        const emptyWeek = dataHandler.initializeEmptyWeek();
-    
-        expect(emptyWeek.weekNumber).toEqual(dateTime.weekNumber)
-        expect(emptyWeek.loggedTimes.length).toEqual(7);
-        expect(emptyWeek.loggedTimes[1].day).toBe("Tuesday");
-
-        // {
-        //     weekNumber: x,
-        //     loggedTimes: [
-        //         {
-        //             day: Sunday,
-        //             id: 12112021,
-        //             dateTimeObj: dt,
-        //             calendarDate: 21,
-        //             offset: currentDayNumberOfTheWeek - thisDayNumberOfTheWeek
-        //         }, 
-        //         {
-        //             day: Monday,
-        //             // ...
-        //         }
-        //     ]
-        // }
-    })
-})
+    });
+});
