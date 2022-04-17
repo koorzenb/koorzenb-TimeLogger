02920b5672131b19b209d957a18ee2d4711fe9ce
Barend Koorzen <koorzenb@gmail.com>
2022-04-17 13:21:48 +0200

    wiring up button

--- src/viewModel.js
+++ src/viewModel.js
@@ -1,8 +1,26 @@
 import {DataStore} from './lib/dataStore.js';
+import {registerEvent, unregisterEvents} from './utils/system-utils.js';
 export class ViewModel {
 
     constructor() {
         console.log("viewModel started");
         const dataStore = new DataStore("toDoApp");
+        this.addButton = document.getElementById("add-item");
+        registerEvent(this.addButton, "click", this.addItem.bind(this));
+    }
+
+    dispose() {
+        this.addButton = null;
+        unregisterEvents(this.addButton, "click");
+    }
+
+    addItem() {
+        event.preventDefault();
+        const item = document.getElementById("item-input").value;
+        const itemObj = {
+            item: item,
+            completed: false
+        };
+        window.eventEmitter.emit("create-record", itemObj);
     }
 }
